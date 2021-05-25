4532e2d00c23268d03fb830040980cb75d200d72
SadraBoromand <sadrabroo@gmail.com>
2021-05-25 09:36:34 +0430

    Init Project

---
+++ Accounting.App/Customers/frmCustomer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accounting.App.Customers;
+using Accounting.DataLayer.Context;
+
+namespace Accounting.App
+{
+    public partial class frmCustomer : Form
+    {
+        public frmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomer_Load(object sender, EventArgs e)
+        {
+            bindGrid();
+        }
+
+        void bindGrid()
+        {
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                dgvCustomers.AutoGenerateColumns = false;
+                dgvCustomers.DataSource = db.CustomerRepository.GetAllCustomers();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            txtFilter.Text = "";
+            bindGrid();
+        }
+
+        private void txtFilter_TextChanged(object sender, EventArgs e)
+        {
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                dgvCustomers.DataSource = db.CustomerRepository.GetCustomersByFilter(txtFilter.Text);
+            }
+        }
+
+        private void btnDeleteCustomer_Click(object sender, EventArgs e)
+        {
+
+            if (dgvCustomers.CurrentRow != null)
+            {
+                using (UnitOfWork db = new UnitOfWork())
+                {
+                    string name = dgvCustomers.CurrentRow.Cells[1].Value.ToString();
+                    if (RtlMessageBox.Show($"آیا از حذف {name} مطمئن هستید ؟", "توجه", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    {
+
+                        int customerId = int.Parse(dgvCustomers.CurrentRow.Cells[0].Value.ToString());
+                        db.CustomerRepository.DeleteCustomer(customerId);
+                        db.Save();
+                        bindGrid();
+
+                    }
+                }
+            }
+            else
+            {
+                RtlMessageBox.Show("لطفا شخصی را انتخاب کنید");
+            }
+        }
+
+        private void btnAddNewCustomer_Click(object sender, EventArgs e)
+        {
+            frmAddOrEditCustomer frmAdd = new frmAddOrEditCustomer();
+            if (frmAdd.ShowDialog() == DialogResult.OK)
+            {
+                bindGrid();
+            }
+        }
+
+        private void btnEditCustomer_Click(object sender, EventArgs e)
+        {
+            if (dgvCustomers.CurrentRow != null)
+            {
+                int customerId = int.Parse(dgvCustomers.CurrentRow.Cells[0].Value.ToString());
+                frmAddOrEditCustomer frmAddOrEditCustomer = new frmAddOrEditCustomer();
+                frmAddOrEditCustomer.customerId = customerId;
+                if (frmAddOrEditCustomer.ShowDialog() == DialogResult.OK)
+                {
+                    bindGrid();
+                }
+            }
+        }
+    }
+}
---
+++ Accounting.Utility/Convertor/DateConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Accounting.Utility.Convertor
+{
+    public static class DateConvertor
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            return
+                $"{persianCalendar.GetYear(value)}/{persianCalendar.GetMonth(value).ToString("00")}/{persianCalendar.GetDayOfMonth(value).ToString("00")}";
+        }
+
+        public static DateTime ToMiladi(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year,dateTime.Month,dateTime.Day,new PersianCalendar());
+        }
+    }
+}
